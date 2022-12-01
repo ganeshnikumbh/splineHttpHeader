940265c28508fdcc4cedd748e645d959454f7a2c
Ganesh Nikumbh <C83775@launcher.int.abnamro.com>
2022-12-01 16:59:15 +0100

    Added intial Code

---
+++ src/main/scala/org/abnamro/dmp/mdc/splinecustomhttpdispatcher/Calculator.scala
@@ -0,0 +1,8 @@
+package org.abnamro.dmp.mdc.splinecustomhttpdispatcher
+
+class Calculator {
+  def add(a: Int, b: Int) = a + b
+  def subtract(a: Int, b: Int) = a - b
+  def multiply(a: Int, b: Int) = a * b
+  def divide(a: Int, b: Int) = a / b
+}
